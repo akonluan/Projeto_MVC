c04ace11356b07c09739548e0753ea965f6a0a29
akonluan <akon.carregosa@aluno.senai.br>
2026-04-27 20:00:32 -0300

    V2

---
+++ WebApplication1/WebApplication1/Controllers/EstadoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class EstadoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detalhes()
+        {
+            return View();
+        }
+    }
+}
