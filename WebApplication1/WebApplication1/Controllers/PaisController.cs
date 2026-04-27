4dcc4fc2004b94dc07333bbfdb18185e0c66b3ff
akonluan <akon.carregosa@aluno.senai.br>
2026-04-27 19:16:37 -0300

    V1

---
+++ WebApplication1/WebApplication1/Controllers/PaisController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PaisController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detalhes()
+        {
+            return View();
+        }
+    }
+}
